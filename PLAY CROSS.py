e078729eb924f88bd419df8ef4b202d79d7be904
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2024-10-17 23:28:17 +0300

    Рефакторинг2(оптимизация main, вынос проверки ввода в отдельную функцию и тд.)

--- PLAY CROSS.py
+++ PLAY CROSS.py
@@ -1,70 +1,95 @@
 import time
 
 
-def make_field(field):
+def make_field(field: list):
     """Создаёт поле и выводит его в консоль"""
-    print("*************")
+    print("-------------")
     for i in range(3):
         print("|", field[0 + i * 3], "|", field[1 + i * 3], "|", field[2 + i * 3], "|")
-        print("*************")
+        print("-------------")
 
 
-def start_game(char, field):
-    """Принимает данные, которые ввёл игроки"""
-    while True:
-        position = int(input("Куда поставим " + char + "? "))
+def check_input(position: str):
+    """Проверяет ввод на корректность"""
+    if position.isdigit():
+        position = int(position)
         if 1 <= position <= 9:
+            return True
+        return False
+
+
+def user_input(char: str, field: list, tries: int):
+    """Принимает данные и проверяет позицию"""
+    k = 0
+    while tries > 0:
+        position = input("Куда поставим " + char + "? ")
+        if check_input(position):
+            position = int(position)
             if str(field[position - 1]) not in "XO":
                 field[position - 1] = char
                 break
             else:
-                print("Эта позиция уже занята.")
+                print("Эта позиция уже занята.\n")
         else:
-            print("Некорректный ввод!!! Что бы походить нужно ввести число от 1 до 9.")
+            print("\nНекорректный ввод!!! Что бы походить нужно ввести ЧИСЛО от 1 до 9.")
+        tries -= 1
+        k += 1
+        if tries != 0:
+            print(f"У вас осталось {tries} попыток."
+                  f"\nПо истечению попыток вы будете заблокированы за попытку поломать игру!!!\n")
+        else:
+            print(f"У вас осталось {tries} попыток.")
+    return k
 
 
-def check_win(field):
+def check_win(field: list):
     """Проверяет выигрыш"""
     win_combinations = ((0, 1, 2), (0, 3, 6), (3, 4, 5), (1, 4, 7), (2, 4, 6), (6, 7, 8), (0, 4, 8), (2, 5, 8))
     for x in win_combinations:
         if field[x[0]] == field[x[1]] == field[x[2]]:
-            return field[x[0]]
+            if field[x[0]] == 'X':
+                return "\nВыиграл первый игрок!!!"
+            else:
+                return "\nВыиграл второй игрок!!!"
     return False
 
 
-def secundomer(f):
+def secundomer(func):
     def wrapper():
         start = time.time()
-        f()
+        func()
         end = time.time()
-        print(f'Время: {round(end - start, 1)} сек длилась партия')
+        print(f'Время: {round(end - start, 1)} сек длилась партия.')
 
     return wrapper
 
 
 @secundomer
 def main():
-    """Процесс игры"""
+    """Головная функция, объединяет все функции; описывает весь процесс игры"""
     print("*" * 1, "Игра Крестики-нолики(размер поля 3х3,стандартное)", "*" * 1)
     field = list(range(1, 10))
-    cnt = 0
+    tries, move = 10, 0
     while True:
         make_field(field)
-        if cnt % 2 == 0:
-            start_game("X", field)
-        else:
-            start_game("O", field)
-        cnt += 1
-        if cnt == 9:
-            print("Ничья!")
+        char = 'X' if move % 2 == 0 else 'O'
+        s = user_input(char, field, tries)
+        tries -= s
+        move += 1
+        if tries == 0:
+            print("Вы были заблокированы.")
             break
-        elif 4 < cnt < 9:
-            char = check_win(field)
-            if char == 'X':
-                print("Выиграл первый игрок!!!")
-                break
-            elif char == 'O':
-                print("Выиграл второй игрок!!!")
+        elif move == 9:
+            res = check_win(field)
+            if not res:
+                print("\nНичья!")
+            elif isinstance(res, str):
+                print(res)
+            break
+        elif 4 < move:
+            res = check_win(field)
+            if isinstance(res, str):
+                print(res)
                 break
     make_field(field)
 
